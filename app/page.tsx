4bec2e80bbe1907b98acfcabe712602fb2576ab5
Sam El-Kamand <sam.elkamand@gmail.com>
2024-07-10 08:12:20 +1000

    fix: updated call to action import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import CallToAction from "../components/ui/CallToAction";
+import CallToAction from "../components/ui/calltoaction";
 export default function Home() {
   return (
     <CallToAction></CallToAction>
