d17beb25bec63a096124d842be31a52b567f366e
Sam El-Kamand <sam.elkamand@gmail.com>
2024-07-14 09:27:57 +1000

    feat: added mutation count fail page

--- app/workflow/screen/mutationcount/page.tsx
+++ app/workflow/screen/mutationcount/page.tsx
@@ -9,18 +9,18 @@ export default function Home() {
 
                 <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-1xl sm:p-20">
                     <h1 className="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 pb-4 pt-0">Are there enough mutations in your sample?</h1>
-                    <p className="text-center pb-8">Attempting to interpret mutational signatures results for samples with low mutation counts is unreliable. Ensure your sample has sufficient mutations before proceeding.</p>
+                    <p className="text-center pb-8">Interpreting mutational signatures for samples with low mutation counts is unreliable. Ensure your sample has a sufficient number of mutations before proceeding.</p>
                     <hr className="" />
                     <div className="grid sm:grid-cols-3 gap-4">
                         <div className="p-8 flex flex-col items-center text-center">
                             <h1 className="font-bold text-blue-500 text-lg">Sufficient Mutations</h1>
-                            <p className="text-sm font-light text-gray-400 pt-4 pb-4">This patient has lots of mutations</p>
+                            <p className="text-sm font-light text-gray-400 pt-4 pb-4">This patient has a large number of mutations. </p>
                             <Link href="/workflow/screen/fit"><button className="bg-blue-500 text-white font-bold py-2 px-4 hover:scale-110 duration-75">{'->'}</button></Link>
                         </div>
                         <div className="p-8 flex flex-col items-center text-center">
                             <h1 className="font-bold text-blue-500 text-lg">Quiet Tumour</h1>
                             <p className="text-sm font-light text-gray-400 pt-4 pb-4">Tumour has very few mutations</p>
-                            <Link href=""><button className="bg-blue-500 text-white font-bold py-2 px-4 hover:scale-110 duration-75">{'->'}</button></Link>
+                            <Link href="/workflow/screen/mutationcountfail"><button className="bg-blue-500 text-white font-bold py-2 px-4 hover:scale-110 duration-75">{'->'}</button></Link>
                         </div>
                         <div className="p-8 flex flex-col items-center text-center">
                             <h1 className="font-bold text-blue-500 text-lg">Unsure</h1>
---
+++ app/workflow/screen/mutationcountfail/page.tsx
@@ -0,0 +1,23 @@
+import TextLink from "@/components/ui/textlink"
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons/faTriangleExclamation";
+export default function Home() {
+    return (
+
+        <div>
+            <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+
+                <div className="relative px-4 py-10 bg-orange-100 shadow-lg sm:rounded-1xl sm:p-20">
+                    <h1 className="mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 pb-4 pt-0">Mutational signatures can not be interpreted reliably</h1>
+                    <p className="text-center pb-8">If your sample has too few mutations, signature analysis is unlikely to be reliable or interpretable. Many model-independent analyses (e.g. similar sample searches and motif analysis) may also be unreliable. </p>
+                    <hr className="h-0.5 mx-auto my-2 bg-black border-0 rounded md:my-10"></hr>
+                    <p className="text-center font-bold text-xl pt-4 pb-2">Next Steps</p>
+                    <p className="text-center pb-8">Consider exploring other types of signatures. For example, if you have a low number of single base substitutions (SBS), SBS signature analysis may not be beneficial, but you might have enough insertions and deletions (INDELs) for indel signature analysis or enough large rearrangements for structural variant signature analysis.</p>
+                    {/* <p className="text-center pt-4"><FontAwesomeIcon icon={faTriangleExclamation} className="text-orange-300 text-4xl"></FontAwesomeIcon></p> */}
+
+                </div>
+            </div>
+        </div>
+    );
+}
