8eaddef11fdd1ea535bb5c68676606e5ba39ec44
Sam El-Kamand <sam.elkamand@gmail.com>
2024-07-15 10:09:19 +1000

    fix: improved mutationcount fail page

--- app/workflow/screen/mutationcountfail/page.tsx
+++ app/workflow/screen/mutationcountfail/page.tsx
@@ -13,7 +13,10 @@ export default function Home() {
                     <p className="text-center pb-8">If your sample has too few mutations, signature analysis is unlikely to be reliable or interpretable. Many model-independent analyses (e.g. similar sample searches and motif analysis) may also be unreliable. </p>
                     <hr className="h-0.5 mx-auto my-2 bg-black border-0 rounded md:my-10"></hr>
                     <p className="text-center font-bold text-xl pt-4 pb-2">Next Steps</p>
-                    <p className="text-center pb-8">Consider exploring other types of signatures. For example, if you have a low number of single base substitutions (SBS), SBS signature analysis may not be beneficial, but you might have enough insertions and deletions (INDELs) for indel signature analysis or enough large rearrangements for structural variant signature analysis.</p>
+                    <p className="text-center pb-8">Consider exploring other types of signatures. For instance, a sample can contain too few single base substitutions (SBS)  for SBS signature analysis to be useful, but contain sufficient insertions and deletions (INDELs) for indel signature analysis, or large rearrangements for structural variant signature analysis.
+                        <br /><br />
+                        If the tumour has very few mutations across all genomic variant types, it could indicate that tumorigenesis was not contingent on mechanisms that increase mutation rate.
+                    </p>
                     {/* <p className="text-center pt-4"><FontAwesomeIcon icon={faTriangleExclamation} className="text-orange-300 text-4xl"></FontAwesomeIcon></p> */}
 
                 </div>
